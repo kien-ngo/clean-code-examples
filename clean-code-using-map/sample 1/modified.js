a02681e26d66f0b4fb4b7481e09c471b1145748c
Kien Ngo <kenseverus@gmail.com>
2021-06-25 11:21:33 -0400

    clean-code-using-map/sample 1

---
+++ clean-code-using-map/sample 1/modified.js
@@ -0,0 +1,21 @@
+const dataset = {
+    Customize: ['customize'],
+    Centers: ['center_center', 'top_center', 'bottom_center', 'front_center', 'back_center', 'left_center', 'right_center'],
+    Corners: ['top_corner_1', 'top_corner_2', 'top_corner_3', 'top_corner_4', 'bottom_corner_1', 'bottom_corner_2', 'bottom_corner_3', 'bottom_corner_4'],
+    Sides: ['top_front_center', 'top_back_center', 'top_left_center', 'top_right_center', 'bottom_front_center', 'bottom_back_center', 'bottom_left_center', 'bottom_right_center', 'front_left_center', 'front_right_center', 'back_left_center', 'back_right_center']
+};
+return Object.keys(dataset).map(categoryName => {
+    const item = {
+        category: categoryName,
+        isExpanded: true,
+        presets: []
+    };
+    item.presets = dataset[categoryName].map(preset => {
+        return {
+            imgSrc: `../WebGLContent/images/pivot_point_templates/${preset}.png`,
+            value: preset.split('_').join('-'),
+            label: preset.split('_').map(name => name.charAt(0).toUpperCase() + name.slice(1)).join(' ')
+        }
+    });
+    return item;
+});
---
+++ clean-code-using-map/sample 1/original.js
@@ -0,0 +1,166 @@
+$rootScope.pivotPointData = [
+    {
+        category: 'Customize',
+        isExpanded: true,
+        presets: [
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/customize.png',
+                value: 'customize',
+                label: 'Customize'
+            }
+        ]
+    },
+    {
+        category: 'Centers',
+        isExpanded: true,
+        presets: [
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/center_center.png',
+                value: 'center-center',
+                label: 'Center Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/top_center.png',
+                value: 'top-center',
+                label: 'Top Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/bottom_center.png',
+                value: 'bottom-center',
+                label: 'Bottom Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/front_center.png',
+                value: 'front-center',
+                label: 'Front Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/back_center.png',
+                value: 'back-center',
+                label: 'Back Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/left_center.png',
+                value: 'left-center',
+                label: 'Left Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/right_center.png',
+                value: 'right-center',
+                label: 'Right Center'
+            }
+        ]
+    },
+    {
+        category: 'Corners',
+        isExpanded: true,
+        presets: [
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/top_corner_1.png',
+                value: 'top-corner-1',
+                label: 'Top Corner 1'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/top_corner_2.png',
+                value: 'top-corner-2',
+                label: 'Top Corner 2'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/top_corner_3.png',
+                value: 'top-corner-3',
+                label: 'Top Corner 3'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/top_corner_4.png',
+                value: 'top-corner-4',
+                label: 'Top Corner 4'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/bottom_corner_1.png',
+                value: 'bottom-corner-1',
+                label: 'Bottom Corner 1'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/bottom_corner_2.png',
+                value: 'bottom-corner-2',
+                label: 'Bottom Corner 2'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/bottom_corner_3.png',
+                value: 'bottom-corner-3',
+                label: 'Bottom Corner 3'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/bottom_corner_4.png',
+                value: 'bottom-corner-4',
+                label: 'Bottom Corner 4'
+            }
+        ]
+    },
+    {
+        category: 'Sides',
+        isExpanded: true,
+        presets: [
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/top_front_center.png',
+                value: 'top-front-center',
+                label: 'Top Front Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/top_back_center.png',
+                value: 'top-back-center',
+                label: 'Top Back Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/top_left_center.png',
+                value: 'top-left-center',
+                label: 'Top Left Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/top_right_center.png',
+                value: 'top-right-center',
+                label: 'Top Right Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/bottom_front_center.png',
+                value: 'bottom-front-center',
+                label: 'Bottom Front Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/bottom_back_center.png',
+                value: 'bottom-back-center',
+                label: 'Bottom Back Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/bottom_left_center.png',
+                value: 'bottom-left-center',
+                label: 'Bottom Left Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/bottom_right_center.png',
+                value: 'bottom-right-center',
+                label: 'Bottom Right Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/front_left_center.png',
+                value: 'front-left-center',
+                label: 'Front Left Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/front_right_center.png',
+                value: 'front-right-center',
+                label: 'Front Right Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/back_left_center.png',
+                value: 'back-left-center',
+                label: 'Back Left Center'
+            },
+            {
+                imgSrc: '../WebGLContent/images/pivot_point_templates/back_right_center.png',
+                value: 'back-right-center',
+                label: 'Back Right Center'
+            }
+        ]
+    }
+];
